390a43a3b7ac6196bc259077273e226aeb99b2fe
Gangadhar Pathipaka <gangadharpathipaka@Gangadhars-MacBook-Pro.local>
2019-03-12 21:58:03 -0400

    GO Nats demo

---
+++ client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	pb "nats_go_demo/order"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/nats-io/go-nats"
+)
+
+func main() {
+	//Create NATS Server Connection
+	natsConn, _ := nats.Connect(nats.DefaultURL)
+	defer natsConn.Close()
+	log.Println("Connected to the nats server....", nats.DefaultURL)
+	msg, err := natsConn.Request("Discovery.OrderService", nil, 1000*time.Microsecond)
+	log.Println(err)
+	if err == nil && msg != nil {
+		log.Println("*************")
+		orderServiceDiscovery := pb.ServiceDiscovery{}
+		err := proto.Unmarshal(msg.Data, &orderServiceDiscovery)
+		if err != nil {
+			log.Println("error ", err)
+		}
+		log.Println("Message Received ", orderServiceDiscovery.OrderServiceUri)
+	}
+	log.Println("Exiting client....")
+}
---
+++ discovery/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"runtime"
+
+	"github.com/nats-io/go-nats"
+
+	pb "nats_go_demo/order"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	natsConn, _ := nats.Connect(nats.DefaultURL)
+	log.Println("connect to the ", nats.DefaultURL)
+
+	natsConn.Subscribe("Discovery.OrderService", func(m *nats.Msg) {
+		orderServDiscovery := &pb.ServiceDiscovery{OrderServiceUri: "http://localhost:80/OrderServiceUri"}
+		data, err := proto.Marshal(orderServDiscovery)
+		if err == nil {
+			natsConn.Publish(m.Reply, data)
+		}
+	})
+	runtime.Goexit()
+}
